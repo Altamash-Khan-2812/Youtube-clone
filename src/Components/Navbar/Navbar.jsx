f226e20683ed61a646143669393cfe73c3aa2b69
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2025-05-02 23:32:32 +0530

    Initial Commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Navbar.css";
+import menu_icon from "../../assets/menu.png";
+import logo from "../../assets/logo.png";
+import search_icon from "../../assets/search.png";
+import upload_icon from "../../assets/upload.png";
+import more_icon from "../../assets/more.png";
+import notification_icon from "../../assets/notification.png";
+import profile_icon from "../../assets/ayan.jpg";
+
+const Navbar = ({ setSidebar, sidebar }) => {
+  return (
+    <nav className="flex-div">
+      <div className="nav-left flex-div">
+        <img
+          className="menu-icon"
+          src={menu_icon}
+          alt="Menu icon"
+          onClick={() => setSidebar((prev) => (prev === false ? true : false))}
+        />
+        <img className="logo" src={logo} alt="Youtube logo" />
+      </div>
+
+      <div className="nav-middle flex-div">
+        <div className="search-box flex-div">
+          <input type="text" placeholder="Search" />
+          <img src={search_icon} alt="Search icon" />
+        </div>
+      </div>
+
+      <div className="nav-right flex-div">
+        <img src={upload_icon} alt="Upload icon" />
+        <img src={more_icon} alt="More options icon" />
+        <img src={notification_icon} alt="Notifications icon" />
+        <img src={profile_icon} className="user-icon" alt="User profile" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
