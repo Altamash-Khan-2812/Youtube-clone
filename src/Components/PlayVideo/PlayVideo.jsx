65045a55ed5d0a0a3169d24b8db721d559e6ad82
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2025-05-05 00:33:26 +0530

    Completed PlayVideo Component

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,10 +1,147 @@
-import React from 'react'
-import "./PlayVideo.css"
+import React from "react";
+import "./PlayVideo.css";
+import video1 from "../../assets/video.mp4";
+import like from "../../assets/like.png";
+import dislike from "../../assets/dislike.png";
+import share from "../../assets/share.png";
+import save from "../../assets/save.png";
+import jack from "../../assets/jack.png";
+import user_profile from "../../assets/user_profile.jpg";
 
 const PlayVideo = () => {
   return (
-    <div>PlayVideo</div>
-  )
-}
+    <div className="play-video">
+      <video src={video1} controls autoPlay></video>
+      <h3>Best Youtube Channel To Learn Web Development</h3>
+      <div className="play-video-info">
+        <p> 2331 views &bull; 2 days ago</p>
+        <div>
+          <span>
+            <img src={like} alt="" />
+            125
+          </span>
+          <span>
+            <img src={dislike} alt="" />2
+          </span>
+          <span>
+            <img src={share} alt="" />
+            Share
+          </span>
+          <span>
+            <img src={save} alt="" />
+            Save
+          </span>
+        </div>
+      </div>
+      <hr />
 
-export default PlayVideo
+      <div className="publisher">
+        <img src={jack} alt="" />
+        <div>
+          <p>GreatStack</p>
+          <span>1M Subscribers</span>
+        </div>
+        <button>Subscribe</button>
+      </div>
+
+      <div className="video-description">
+        <p>Channel that makes learning easy</p>
+        <p>Subscribe GreatStack to watch more Tutorials on web development</p>
+        <hr />
+        <h4>130 Comments</h4>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Jack nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+              eaque blanditiis libero. Hic dolore, ea ipsa explicabo distinctio
+              officiis perspiciatis!
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>244</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Jack nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+              eaque blanditiis libero. Hic dolore, ea ipsa explicabo distinctio
+              officiis perspiciatis!
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>244</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Jack nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+              eaque blanditiis libero. Hic dolore, ea ipsa explicabo distinctio
+              officiis perspiciatis!
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>244</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Jack nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+              eaque blanditiis libero. Hic dolore, ea ipsa explicabo distinctio
+              officiis perspiciatis!
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>244</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Jack nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+              eaque blanditiis libero. Hic dolore, ea ipsa explicabo distinctio
+              officiis perspiciatis!
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>244</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlayVideo;
